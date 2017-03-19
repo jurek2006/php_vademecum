1c03c1bc1dac23e41bab716db967c0b2bab58625
jurek2006 <jurek2006@gmail.com>
2017-03-19 16:06:16 +0100

    Plik początkowy

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>PHP Vademecum</title>
+  <!-- <meta name="description" content="The HTML5 Herald"> -->
+  <meta name="author" content="Jurek">
+
+  <!-- <link rel="stylesheet" href="css/styles.css?v=1.0"> -->
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<h1>PHP Vademecum</h1>
+<p>Ćwiczenia do książki</p> 
+</body>
+</html>
