fe47bfd7c93f91e64db2d79ee9b182355063690f
jurek2006 <jurek2006@gmail.com>
2017-03-23 11:52:36 +0100

    Poprawki

--- php/r04/processfeedback.php
+++ php/r04/processfeedback.php
@@ -1,17 +1,17 @@
 <?php 
 	// utworzenie krótkich nazw zmiennych
-	$name = $_POST['name'];
-	$email = $_POST['email'];
-	$feedback = $_POST['feedback'];
+	$name = trim($_POST['name']);
+	$email = trim($_POST['email']);
+	$feedback = trim($_POST['feedback']);
 
 	// ustawienie stałych danych
 	$toaddress = "jurek2006@gmail.com";
 
-	$subject = "Feedback from web site";
+	$subject = "Feedback from web site"; 
 
-	$mailcontent = 	"Customer name: " . filter_var($name) . "\n" .
-					"Customer email: " . $email . "\n" .
-					"Customer comments:\n" . $feedback . "\n";
+	$mailcontent = 	"Customer name: " . str_replace("\r\n", "", $name) . "\n" .
+					"Customer email: " . str_replace("\r\n", "", $email) . "\n" .
+					"Customer comments:\n" . str_replace("\r\n", "",$feedback) . "\n";
 
 	$fromaddress = "From: jurek2006@gmail.com";
 
@@ -32,7 +32,8 @@
 <body>
 
 	<h1>Feedback submitted</h1>
-	<p>Your feedback has been sent.</p>
+	<p>Your feedback (shown below) has been sent.</p>
+	<p><?php echo nl2br(htmlspecialchars($feedback)); ?></p>
 
 </body>
 </html>
